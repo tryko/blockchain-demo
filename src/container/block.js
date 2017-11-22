5dd08deb6df3714560d2122b2dc6d86fc6f3e494
tryko <itay83ko@gmail.com>
2017-11-22 06:02:11 +0200

    some css and html to block class

---
+++ src/container/block.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Block extends Component {
+  render() {
+    return (
+      <div className="block">
+        <label>Index</label>
+      </div>
+    );
+  }
+}
+
+export default Block;
